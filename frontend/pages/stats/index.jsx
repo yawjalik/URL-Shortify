cb8781d2fbcd254c6a76177fb41e1e728130cfd9
Yaw Jalik <jalik2129@gmail.com>
2022-02-20 20:57:34 +0800

    Cleaned up stats page

--- frontend/pages/stats/index.jsx
+++ frontend/pages/stats/index.jsx
@@ -4,41 +4,34 @@ import axios from 'axios'
 import NavBar from "../../components/navbar"
 
 const Stats = () => {
-    const [text, setText] = useState("")
-    const [stats, setStats] = useState([{url: "url", datetime_created: "date", shortened_url: "short"}])
+    const [urlInput, setUrlInput] = useState("")
+    const [stats, setStats] = useState([])
     const [loading, setLoading] = useState(false)
 
-
-    const getStats = async () => {
-        const res = await axios.get('http://localhost:8000/stats')
-                                .then(res => res.data)
-                                .catch(() => toast.warning("No stats found"))
-        console.log(res.data)
-        setStats(res.data)
-        console.log(stats)
-    }
-
     useEffect(() => {
-        // getStats()
-        console.log(stats)
+        const getStats = async () => {
+            const res = await axios.get('http://localhost:8000/stats')
+                                    .then(res => res.data)
+                                    .catch(() => toast.warning("No stats found"))
+            console.log(res.data)
+            setStats(res.data)
+            console.log(stats)
+        }
+        getStats()
     }, [])
-    
 
     const onClick = () => {
-        if (text.length > 0) {
-            setLoading(true)
-            axios.get('http://localhost:8000/')
-        } 
-        else {
-            getStats()
-            console.log(stats)
+        if (urlInput.length > 0) {
+            // setLoading(true)
+            // axios.get(`http://localhost:8000/${urlInput}`)
+            //     .then(res => res.data)
+            //     .catch(() => toast.warning("No records found"))
         }
     }
 
     return <div className="text-center">
         <h1 className='text-4xl font-bold my-2'>Stats</h1>
         <NavBar/>
-        
 
         <table className='table-auto divide-y divide-gray-200 w-full'>
             <thead className="bg-gray-50">
@@ -72,7 +65,7 @@ const Stats = () => {
             </tbody>
         </table>
 
-        <input className='bg-gray-200 rounded px-2 py-1 m-2' type="text" placeholder='Search URL stats by hash' value={text} onChange={e => setText(e.target.value)}/>
+        <input className='bg-gray-200 rounded px-2 py-1 m-2' type="text" placeholder='Search URL stats by hash' value={urlInput} onChange={e => setUrlInput(e.target.value)}/>
         <button disabled={loading} className="bg-blue-200 hover:bg-blue-300 rounded px-2 py-1 m-2" onClick={onClick}>
             {loading ? "Loading" : "Search"}
         </button>
